80b2134f45e4c2d59a24797cc7fc55019e347aa6
Mariana Goncalves <mari.goncalves004@gmail.com>
2025-05-14 20:59:12 -0300

    Ajuste de estilo

--- busca-cep/src/App.jsx
+++ busca-cep/src/App.jsx
@@ -40,13 +40,25 @@ function App() {
             </button>
           </section>
 
-          <section className="address_data">
-            <p><strong>CEP:</strong> {data.cep}</p>
-            <p><strong>Estado:</strong> {data.estado} </p>
-            <p><strong>Cidade:</strong> {data.localidade}</p>
-            <p><strong>Logradouro:</strong> {data.logradouro}</p>
-            <p><strong>Bairro:</strong> {data.bairro}</p>
-          </section>
+          {Object.keys(data).length > 0 && (
+            <section className="address_data">
+              <p>
+                <strong>CEP:</strong> {data.cep}
+              </p>
+              <p>
+                <strong>Estado:</strong> {data.estado}{" "}
+              </p>
+              <p>
+                <strong>Cidade:</strong> {data.localidade}
+              </p>
+              <p>
+                <strong>Logradouro:</strong> {data.logradouro}
+              </p>
+              <p>
+                <strong>Bairro:</strong> {data.bairro}
+              </p>
+            </section>
+          )}
         </div>
       </div>
     </main>
