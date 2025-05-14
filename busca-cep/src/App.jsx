07022864fc2e648e3bad06bbe8c727be4f9c44d9
Mariana Goncalves <mari.goncalves004@gmail.com>
2025-05-13 21:48:47 -0300

    Criação da estilização base dos elementos

--- busca-cep/src/App.jsx
+++ busca-cep/src/App.jsx
@@ -1,10 +1,32 @@
-function App() {
+import "../src/app.css";
 
+function App() {
   return (
-    <>
-    teste
-    </>
-  )
+    <main className="app">
+      <div className="app_container">
+        <h1 className="title">Busca CEP</h1>
+
+        <section className="search_container">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="search_input"
+            placeholder="digite seu CEP"
+          />
+          <button className="search_button">Pesquisar</button>
+        </section>
+
+        <section className="address_data">
+          <p>CEP: </p>
+          <p>Estado: </p>
+          <p>Cidade: </p>
+          <p>Logradouro: </p>
+          <p>Bairro: </p>
+        </section>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
