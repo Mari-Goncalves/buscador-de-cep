a18a640aabd9839b2a5f480197d8907253b6dac8
Mariana Goncalves <mari.goncalves004@gmail.com>
2025-05-14 20:46:52 -0300

    Criação do layout do campo de pesquisa

--- busca-cep/src/App.jsx
+++ busca-cep/src/App.jsx
@@ -26,28 +26,28 @@ function App() {
       <div className="app_container">
         <h1 className="title">Busca CEP</h1>
 
-        <section className="search_container">
-          <input
-            type="text"
-            className="search_input"
-            placeholder="digite seu CEP"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <button className="search_button" onClick={searchCEP}>
-            Pesquisar
-          </button>
-        </section>
-
-        {Object.keys(data).length > 0 && (
-          <section className="address_data">
-            <p>CEP: {data.cep}</p>
-            <p>Estado: {data.estado} </p>
-            <p>Cidade: {data.localidade}</p>
-            <p>Logradouro: {data.logradouro}</p>
-            <p>Bairro: {data.bairro}</p>
+        <div className="main_content">
+          <section className="search_container">
+            <input
+              type="text"
+              className="search_input"
+              placeholder="Digite seu CEP"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button className="search_button" onClick={searchCEP}>
+              Pesquisar
+            </button>
           </section>
-        )}
+
+          <section className="address_data">
+            <p><strong>CEP:</strong> {data.cep}</p>
+            <p><strong>Estado:</strong> {data.estado} </p>
+            <p><strong>Cidade:</strong> {data.localidade}</p>
+            <p><strong>Logradouro:</strong> {data.logradouro}</p>
+            <p><strong>Bairro:</strong> {data.bairro}</p>
+          </section>
+        </div>
       </div>
     </main>
   );
